300abf3974130646a778cab3b29d9e9cf395a42f
sanchitd5 <sanchitd5@live.com>
2019-07-22 13:53:48 +1000

    App.js: BoilerPlate Setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+/***
+ *  Created by Sanchit Dang
+ ***/
+import React, { useEffect } from 'react';
+import './styles/App.scss';
+import './database/idb'
+import { CssBaseline, Container } from '@material-ui/core'
+import { AppRoutes } from './routes/routes';
+import { ContextManager } from 'contexts'
+import { Notification } from 'components'
 
-function App() {
+
+const App = () => {
+  useEffect(() => {
+    document.title = process.env.REACT_APP_NAME;
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to React-Frontend-Boilerplate 0.1.0-alpha
-        </p>
-      </header>
-    </div>
+    <ContextManager>
+      <Container maxWidth="lg">
+        <CssBaseline />
+        <AppRoutes />
+        <Notification />
+      </Container>
+    </ContextManager>
   );
 }
 
