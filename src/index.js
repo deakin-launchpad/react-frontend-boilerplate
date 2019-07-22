5bc01d25eecd803942996b657ee5ed40fc4d0e27
sanchitd5 <sanchitd5@live.com>
2019-07-22 13:53:27 +1000

    Src/index.js: Add Router Globally

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './styles/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const customHistory = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={customHistory}>
+    <App />
+  </Router>
+  , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
