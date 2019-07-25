4649f32144f227222bab7fb1f0c589019f327d1c
sanchitd5 <sanchitd5@live.com>
2019-07-26 09:57:19 +1000

    Dummy API for login

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,7 +1,12 @@
-import { AccessToken } from '../components/helpers'
+import { AccessToken } from 'contexts/helpers'
 
 export default class API {
   displayAccessToken = () => {
     console.log(AccessToken)
+  }
+
+  login = (data, callback) => {
+    if (data)
+      callback(true)
   }
 }
