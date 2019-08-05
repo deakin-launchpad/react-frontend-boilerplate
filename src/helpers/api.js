8d157646d5723f37c26f5e726b939b649d0086e5
sanchitd5 <sanchitd5@live.com>
2019-08-05 23:02:56 +1000

    Api.js : Multiple Changes import notify to fix error example of errorHelper

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,14 +1,31 @@
 import { AccessToken } from 'contexts/helpers'
+import { notify } from 'components'
+import { axiosInstance } from './index';
 
+/**
+ *  @errorHelper :  Function to return error StatusText.
+ */
+const errorHelper = (error) => {
+  if (error.response === undefined) {
+    notify("Network Error");
+    return false;
+  }
+  if (error.response.statusText !== "") {
+    notify(error.response.statusText);
+    return false;
+  }
+}
 class API {
   displayAccessToken = () => {
     console.log(AccessToken)
   }
 
   login = (data, callback) => {
-    console.log(data)
-    if (data)
+    axiosInstance.post('login', data).then(response => {
       return callback(true)
+    }).catch(error => {
+      errorHelper(error)
+    })
   }
 }
 const instance = new API();
