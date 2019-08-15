110b6891e8c398a911bfff1acde03d06a43c6724
sanchitd5 <sanchitd5@live.com>
2019-08-15 10:34:17 +1000

    index view : mobileMenu

--- src/views/index.js
+++ src/views/index.js
@@ -3,9 +3,11 @@
  ***/
 import { Login } from './common/Login/Login';
 import { Register } from './common/Register/Register'
-import { Home } from './dependants/Home/Home'
+import { Home } from './dependants/Home/Home';
+import { MobileMenu } from './dependants/mobileMenu/mobileMenu'
 export {
   Login,
   Register,
-  Home
+  Home,
+  MobileMenu
 }
