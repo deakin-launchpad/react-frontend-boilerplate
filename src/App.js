ed11eceb5bfb7d9455221a7d20bec38cb4f32f25
sanchitd5 <sanchitd5@live.com>
2019-08-20 12:09:08 +1000

    AppRoutes : Send routes withRouter

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
  ***/
 import React, { useEffect } from 'react';
 import './styles/App.scss';
+import { withRouter } from 'react-router-dom';
 import './database/idb'
 import { CssBaseline } from '@material-ui/core'
 import { AppRoutes } from './routes/routes';
@@ -10,18 +11,18 @@ import { ContextManager } from 'contexts'
 import { Notification, DevModeSwitch } from 'components'
 import { DevModeConfig } from 'configurations'
 
-const App = () => {
+const App = (props) => {
   useEffect(() => {
     document.title = process.env.REACT_APP_NAME;
   }, []);
   return (
     <ContextManager>
       <CssBaseline />
-      <AppRoutes />
+      <AppRoutes {...props} />
       {DevModeConfig.visible ? <DevModeSwitch /> : ''}
       <Notification />
     </ContextManager>
   );
 }
 
-export default App;
+export default withRouter(App);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,11 +3,11 @@
  ***/
 import React, { useContext, useState, useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import { LoginContext } from 'contexts'
-import { Login, Register, Home, MobileMenu } from 'views'
+import { LoginContext } from 'contexts';
+import { Login, Register, Home, MobileMenu } from 'views';
 import { Layout } from '../layout';
 
-export const AppRoutes = () => {
+export const AppRoutes = (props) => {
   const { loginStatus } = useContext(LoginContext);
   const [redirectToLogin, setRedirectToLogin] = useState(false);
   useEffect(() => {
@@ -15,16 +15,16 @@ export const AppRoutes = () => {
       setRedirectToLogin(false);
     else
       setRedirectToLogin(true);
-  }, [loginStatus])
+  }, [loginStatus]);
   return (
     <Switch>
-      <Route exact path='/' render={() => ((redirectToLogin ? <Redirect to={{ pathname: '/login' }} /> : <Redirect to={{ pathname: '/home' }} />))} />
-      <Route exact path='/login' render={() => ((!redirectToLogin ? <Redirect to={{ pathname: '/home' }} /> : <Login />))} />
-      <Route exact path='/register' render={() => ((!redirectToLogin ? <Redirect to={{ pathname: '/home' }} /> : <Register />))} />
+      <Route exact path='/' render={() => ((redirectToLogin ? <Redirect to={{ pathname: '/login' }} {...props} /> : <Redirect to={{ pathname: '/home' }} {...props} />))} />
+      <Route exact path='/login' render={() => ((!redirectToLogin ? <Redirect to={{ pathname: '/home' }} {...props} /> : <Login  {...props} />))} />
+      <Route exact path='/register' render={() => ((!redirectToLogin ? <Redirect to={{ pathname: '/home' }} {...props} /> : <Register {...props} />))} />
       <Layout>
-        <Route exact path='/home' render={() => ((redirectToLogin ? <Redirect to={{ pathname: '/login' }} /> : <Home />))} />
-        <Route exact path='/menu' render={() => ((redirectToLogin ? <Redirect to={{ pathname: '/login' }} /> : <MobileMenu />))} />
+        <Route exact path='/home' render={() => ((redirectToLogin ? <Redirect to={{ pathname: '/login' }} {...props} /> : <Home {...props} />))} />
+        <Route exact path='/menu' render={() => ((redirectToLogin ? <Redirect to={{ pathname: '/login' }}  {...props} /> : <MobileMenu  {...props} />))} />
       </Layout>
     </Switch>
-  )
+  );
 };
