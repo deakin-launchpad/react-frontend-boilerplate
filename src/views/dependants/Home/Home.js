f1083285ba885b379cf09c810482f4bf440234b9
sanchitd5 <sanchitd5@live.com>
2019-09-06 11:03:19 +1000

    Improve Landing Page

--- src/views/dependants/Home/Home.js
+++ src/views/dependants/Home/Home.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Grid, Typography } from '@material-ui/core';
+import { Image } from 'components';
 
 export const Home = () => {
 
-  return (
-    <div>
-      WELCOME to HOME
-    </div>
-  )
-}
+  return (<Grid container>
+    <Grid item xs={12}>
+      <Image src={'https://upload.wikimedia.org/wikipedia/commons/8/88/Mini-Robot.png'} />
+    </Grid>
+    <Grid item xs={12}>
+      <Typography variant="h5" align="center">
+        Welcome to React Boiler Plate
+      </Typography>
+      <Typography variant="body2" align="center" >
+        This boilerplate is made possible using <a href='https://material-ui.com/' rel="noopener noreferrer" target="_blank">Material-UI</a>
+      </Typography>
+    </Grid>
+  </Grid>);
+};
