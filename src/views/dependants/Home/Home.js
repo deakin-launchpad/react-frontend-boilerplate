05eb2e9398e63531aa73d8989087f3fcce8ea84e
sanchitd5 <sanchitd5@live.com>
2019-09-06 11:15:48 +1000

    Home: Alignment Fixes

--- src/views/dependants/Home/Home.js
+++ src/views/dependants/Home/Home.js
@@ -4,11 +4,11 @@ import { Image } from 'components';
 
 export const Home = () => {
 
-  return (<Grid container>
-    <Grid item xs={12}>
+  return (<Grid container justify='flex-start' direction='column' alignItems='center'>
+    <Grid item xs={12} xl={2} lg={4} md={6} sm={8}>
       <Image src={'https://upload.wikimedia.org/wikipedia/commons/8/88/Mini-Robot.png'} />
     </Grid>
-    <Grid item xs={12}>
+    <Grid item xs={12} xl={12} lg={12} md={12} sm={12}>
       <Typography variant="h5" align="center">
         Welcome to React Boiler Plate
       </Typography>
