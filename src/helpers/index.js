a9363592fc0ea515b243a675e09c5326a8731243
sanchitd5 <sanchitd5@live.com>
2019-09-26 14:42:47 +1000

    Index new helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,7 +1,10 @@
 import { axiosInstance } from './axiosInstance';
 import API from './api';
-
+import TextHelper from './TextHelper';
+import OutputHelper from './OutputHelper';
 export {
   axiosInstance,
-  API
-}
+  API,
+  TextHelper,
+  OutputHelper
+};
