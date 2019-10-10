7a558fb9b7cbba6068f5122a2a9e77520b8ee77a
sanchitd5 <sanchitd5@live.com>
2019-10-10 15:40:30 +1100

    Example.js: Padding Fixes

--- src/views/dependants/Example/Example.js
+++ src/views/dependants/Example/Example.js
@@ -2,23 +2,24 @@ import React, { useState } from 'react';
 import { Grid, Container, Typography, Button, Card, CardContent, CardActions, makeStyles } from '@material-ui/core';
 import { EnhancedEditor } from 'components';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
-    marginTop: '2vh'
+    marginTop: theme.spacing(1),
+    marginLeft: theme.spacing(1)
   }
-})
+}))
 
 export const Example = () => {
   const classes = useStyles();
   const [contentStorage, setContentStorage] = useState('');
   let content = (
-    <Container className={classes.root}>
-      <Grid container alignItems='center'>
-        <Grid item xs={8}>
+    <div className={classes.root}>
+      <Grid container spacing={1} alignItems='center'>
+        <Grid item xs={6}>
           <Card>
             <CardContent>
               <Typography gutterBottom variant="h5" component="h2">Text Editor Example</Typography>
-              <EnhancedEditor id={'textEditor'} getContent={(content) => setContentStorage(content)} options={{ menuBar: false }} />
+              <EnhancedEditor id={'textEditor'} getContent={(content) => setContentStorage(content)} />
             </CardContent>
             <CardActions>
               <Button variant="outlined" onClick={() => console.log(contentStorage)}>Console Data</Button>
@@ -26,6 +27,6 @@ export const Example = () => {
           </Card>
         </Grid>
       </Grid>
-    </Container>);
+    </div>);
   return content;
 };
