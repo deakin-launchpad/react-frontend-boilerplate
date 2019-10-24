18abd465768017bbcc685c3f8b69f153b86f8fae
sanchitd5 <sanchitd5@live.com>
2019-10-24 13:42:04 +0530

    Helpers: add customHook: useLocalStorage

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,11 +2,12 @@ import { axiosInstance } from './axiosInstance';
 import API from './api';
 import TextHelper from './TextHelper';
 import OutputHelper from './OutputHelper';
-import { useKeyPress } from './CustomHooks';
+import { useKeyPress, useLocalStorage } from './CustomHooks';
 export {
   axiosInstance,
   API,
   TextHelper,
   OutputHelper,
-  useKeyPress
+  useKeyPress,
+  useLocalStorage
 };
