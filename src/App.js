7e645d80ac8916c8eaee3df09294bca0fe6b634f
sanchitd5 <sanchitd5@live.com>
2019-11-25 14:40:49 +1100

    Add missing semicolons

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@
 import React, { useEffect } from 'react';
 import './styles/App.scss';
 import { withRouter } from 'react-router-dom';
-import './database/idb'
+import './database/idb';
 import { CssBaseline } from '@material-ui/core'
 import { AppRoutes } from './routes/routes';
-import { ContextManager } from 'contexts'
-import { Notification, DevModeSwitch } from 'components'
-import { DevModeConfig } from 'configurations'
+import { ContextManager } from 'contexts';
+import { Notification, DevModeSwitch } from 'components';
+import { DevModeConfig } from 'configurations';
 
 const App = (props) => {
   useEffect(() => {
