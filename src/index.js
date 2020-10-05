79e1deda7467b45a4a91265706fa64905179e92f
Sanchit Dang <sanchitd5@live.com>
2020-10-05 11:07:11 +1100

    use BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-
-const customHistory = createBrowserHistory();
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <Router history={customHistory}>
+  //uses the HTML5 history API (pushState, replaceState and the popstate event) to keep your UI in sync with the URL.
+  <BrowserRouter>
     <App />
-  </Router>
+  </BrowserRouter>
   , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
