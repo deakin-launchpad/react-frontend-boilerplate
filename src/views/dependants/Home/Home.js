0bf36b36612ea615cc10c5e0c1cb493f3a178538
Monica <monicatasmin06@gmail.com>
2023-01-06 19:40:39 +0700

    frontend merchant

--- src/views/dependants/Home/Home.js
+++ src/views/dependants/Home/Home.js
@@ -1,9 +1,59 @@
-
-import { Box, Container, Typography } from '@mui/material';
+import { useState, useEffect, useCallback } from 'react';
+import { Container, Typography,Box } from '@mui/material';
 import { LayoutConfig } from 'constants/index';
+import { API } from 'helpers';
+import { EnhancedTable, notify } from 'components/index';
+import { useIsMountedRef } from '../../../helpers/hooks/index';
 
 
 export const Home = () => {
+  const [merchants, setMerchants] = useState([]);
+
+  const isMounted = useIsMountedRef();
+
+  const getMerchants = useCallback(async () => {
+    try {
+      const response = await API.getMerchants();
+      if (response.success) {
+        console.log(response.data.data);
+        if (isMounted) setMerchants(response.data.data);
+      }
+      else {
+        setMerchants([]);
+        notify('Failed to Fetch Merchants List');
+      }
+    } catch (err) {
+      setMerchants([]);
+      notify('Failed to Fetch Merchants List');
+    }
+
+  }, [isMounted]);
+
+  useEffect(() => {
+    getMerchants();
+  }, [getMerchants]);
+
+  async function verifyMerchant(verify,merchantId){
+    const data = {
+      "merchantId" : merchantId,
+      "verify" : verify
+    };
+    try {
+      const response = await API.verivyMerchants(data);
+      console.log(response);
+      if (response.success) {
+        console.log(response.data.data);
+      }
+      else {
+        notify('Failed to Fetch Merchants List');
+      }
+    } catch (err) {
+      console.log(err);
+      notify('Failed to Fetch Merchants List');
+    }
+    getMerchants();
+  }
+
   return (<Box sx={LayoutConfig.defaultContainerSX}>
     <Container
       style={{
@@ -40,6 +90,9 @@ export const Home = () => {
       >
         To explore more visit the examples tab on the left.
       </Typography>
+
+      <EnhancedTable data={merchants} title="Merchants" options={{ ignoreKeys: ['_id'],actions: [{name: 'Action', label: 'Verify', type:'button', function: (e, data) => { verifyMerchant(!data.isVerified,data._id); }}], }} />
+       
     </Container>
   </Box>);
 };
